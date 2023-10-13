18fe5afb2e61268f24b300280e8cb591e6119b2a
That-Italian-Guy <106380880+That-Italian-Guy@users.noreply.github.com>
2023-10-13 20:03:15 +0100

    Update targetNearby.js
    Fixed order of logic, fixed allied logic, added explanation to name filters

--- targetNearby.js
+++ targetNearby.js
@@ -3,6 +3,21 @@
 let myToken = canvas.tokens.controlled[0];
 let targets = canvas.tokens.placeables.filter(token => {
 
+    /// Comment out the next three lines to disable specific name filtering. Replace the name with the desired one (must be the token's, not the actor's name!) OR replace <<<tokenName === "Demonic Hound">>> with <<<tokenName === myToken.document.name>>> to target tokens that share the selected token's name.
+    ///let tokenName = token.document.name;
+    ///let isName = (tokenName === "Demonic Hound");
+    ///if (!isName) return false;
+
+    /// *****NOTE: You HAVE TO comment out ONE or BOTH of the following disposition filtering, or the macro will not target ANY token. Comment BOTH out to target EVERY token.*****
+    /// If the next three lines are not commented out, the macro will target ENEMIES (IE: tokens with the opposite Disposition of the selected token; it will exclude Neutral disposition tokens).
+    let disposition = myToken.document.disposition;
+    let isHostile = ((disposition !== token.document.disposition) && (token.document.disposition !== 0));
+    if (!isHostile) return false;
+    /// If the next three lines are not commented out, the macro will target ALLIES (IE: tokens with the same Disposition as the selected token; it will exclude Neutral disposition tokens).
+    ///let disposition = myToken.document.disposition;
+    ///let isAllied = ((disposition === token.document.disposition) && (token.document.disposition !== 0));
+    ///if (!isAllied) return false;
+    
     /// Comment out the next two lines to disable distance filtering. If this is enabled, only tokens within n ft. (horizontally) will be targeted. Change n in "distance >= n" to set the distance. Should be 2ft above the expected value to partially deal with diagonals (IE: if you want 30ft, set the number to 32). 
     let distance = canvas.grid.measureDistance(myToken, token);
     if (distance >= 17) return false;
@@ -12,22 +27,6 @@ let targets = canvas.tokens.placeables.filter(token => {
     let tokenElevation = token.document.elevation;
     let elevationDiff = Math.abs(myElevation - tokenElevation);
     if (isNaN(elevationDiff) || elevationDiff <= 15) return true;
-
-    /// *****NOTE: You HAVE TO comment out ONE or BOTH of the following disposition filtering, or the macro will not target ANY token. Comment BOTH out to target EVERY token.*****
-    
-    /// If the next three lines are not commented out, the macro will target ENEMIES (IE: tokens with the opposite Disposition of the selected token; it will exclude Neutral disposition tokens).
-    let disposition = myToken.document.disposition;
-    let isHostile = ((disposition !== token.document.disposition) && (token.document.disposition !== 0));
-    if (!isHostile) return false;
-    /// If the next three lines are not commented out, the macro will target ALLIES (IE: tokens with the same Disposition as the selected token; it will exclude Neutral disposition tokens).
-    ///let disposition = myToken.document.disposition;
-    ///let isAllied = ((disposition !== token.document.disposition) && (token.document.disposition !== 0));
-    ///if (!isAllied) return false;
-
-    /// Comment out the next three lines to disable name filtering. 
-    ///let tokenName = token.document.name;
-    ///let isName = (tokenName === "Demonic Hound");
-    ///if (!isName) return false;
    
 });
 
